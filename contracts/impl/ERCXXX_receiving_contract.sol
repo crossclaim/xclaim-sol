20cce64ccf1252a3f002475b0b1161b37efe6e95
Dominik Harz <dominik.harz@gmail.com>
2018-07-29 14:48:27 +0100

    Comment out unused contracts

--- contracts/impl/ERCXXX_receiving_contract.sol
+++ contracts/impl/ERCXXX_receiving_contract.sol
@@ -1,16 +1,16 @@
-pragma solidity ^0.4.11;
+// pragma solidity ^0.4.11;
 
- /**
- * @title Contract that will work with ERC223 tokens.
- */
+//  /**
+//  * @title Contract that will work with ERC223 tokens.
+//  */
  
-contract ERCXXXReceivingContract {
-    /**
-    * @dev Standard ERCXXX function that will handle incoming token transfers.
-    *
-    * @param _from  Token sender address.
-    * @param _value Amount of tokens.
-    * @param _data  Transaction metadata.
-    */
-    function tokenFallback(address _from, uint _value, bytes _data);
-}
+// contract ERCXXXReceivingContract {
+//     /**
+//     * @dev Standard ERCXXX function that will handle incoming token transfers.
+//     *
+//     * @param _from  Token sender address.
+//     * @param _value Amount of tokens.
+//     * @param _data  Transaction metadata.
+//     */
+//     function tokenFallback(address _from, uint _value, bytes _data);
+// }
