cb6bc92e386b1fafdab0fa66fa3eff87507c5ca4
Dominik Harz <dominik.harz@gmail.com>
2018-08-01 19:47:37 +0100

    Include csv export for gas cost

--- test/ercxxx_sgx_base.js
+++ test/ercxxx_sgx_base.js
@@ -1,5 +1,11 @@
 const ERCXXX_SGX = artifacts.require("./impl/ERCXXX_SGX.sol");
+
+// Writing experiments data to CSV
+var fs = require("fs");
 var csvWriter = require('csv-write-stream');
+var writer = csvWriter();
+
+// Integrate with bitcoin
 const Client = require('bitcoin-core');
 
 
@@ -11,16 +17,36 @@ contract('ERCXXX_SGX', async (accounts) => {
     const bob = accounts[2];
     const charlie = accounts[3];
 
+    // experiment related vars
+    var authorize_gas;
+    var issue_gas;
+    var transfer_gas;
+    var redeem_gas;
+
+    before('Create writer for experiments', async () => {
+        writer.pipe(fs.createWriteStream(('./experiments/Gas_ERCXXX_SGX.csv')));
+    })
+
+    after('Write experiment data to file', async () => {
+        writer.write(
+            {
+                Authorize: authorize_gas,
+                Issue: issue_gas,
+                Transfer: transfer_gas,
+                Redeem: redeem_gas
+            });
+        writer.end();
+    })
+
     beforeEach('setup contract', async function () {
         btc_erc = await ERCXXX_SGX.new("BTC-ERC", "BTH", 1);
     });
-
 
     it("Authorize issuer", async () => {
         // check if authorize event fired
         let authorize_tx = await btc_erc.authorizeIssuer(issuer, { from: issuer, value: web3.toWei(collateral, "ether") });
         eventFired(authorize_tx, "AuthorizedIssuer");
-        // console.log(authorize_tx.receipt.gasUsed);
+        authorize_gas = authorize_tx.receipt.gasUsed;
 
         // check if issuer list is updated
         let updatedIssuer = await btc_erc.issuer.call();
@@ -63,6 +89,7 @@ contract('ERCXXX_SGX', async (accounts) => {
         // check if issue event is fired
         let issue_tx = await btc_erc.issue(alice, amount, btc_tx);
         eventFired(issue_tx, "Issue");
+        issue_gas = issue_tx.receipt.gasUsed;
 
         balance_alice = await btc_erc.balanceOf.call(alice);
         let updated_balance_alice = balance_alice.toNumber();
@@ -102,6 +129,7 @@ contract('ERCXXX_SGX', async (accounts) => {
         // check if transfer event fired
         let transfer_tx = await btc_erc.transferFrom(alice, bob, 1, {from: alice});
         eventFired(transfer_tx, "Transfer");
+        transfer_gas = transfer_tx.receipt.gasUsed;
 
         // check if balances are updated
         balance_alice = await btc_erc.balanceOf.call(alice);
@@ -135,6 +163,7 @@ contract('ERCXXX_SGX', async (accounts) => {
         // check if redeem event fired
         let redeem_tx = await btc_erc.redeem(alice, amount, btc_redeem_tx, { from: issuer });
         eventFired(redeem_tx, "Redeem");
+        redeem_gas = redeem_tx.receipt.gasUsed;
     });
 
     function eventFired(transaction, eventName) {
