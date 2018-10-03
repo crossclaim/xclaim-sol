c0d7f0382c6b817db9c598cdce99c7d97150a5f4
Dominik Harz <dominik.harz@gmail.com>
2018-10-03 12:26:45 +0900

    demo script

---
+++ test/helpers.js
@@ -0,0 +1,15 @@
+module.exports = {
+    eventFired: function (transaction, eventName) {
+        for (var i = 0; i < transaction.logs.length; i++) {
+            var log = transaction.logs[i];
+            if (log.event == eventName) {
+                // We found the event!
+                assert.isTrue(true);
+            }
+            else {
+                assert.isTrue(false, "Did not find " + eventName);
+            }
+        }
+    },
+};
+
