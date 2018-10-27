15b8dbca3e3b141e003b20508acabd768a468506
Dominik Harz <dominik.harz@gmail.com>
2018-10-27 11:54:26 +0100

    Coniditional network deployment

--- migrations/2_ERCXXX_SGX.js
+++ migrations/2_ERCXXX_SGX.js
@@ -1,5 +1,9 @@
 var ERCXXX_SGX = artifacts.require("./impl/ERCXXX_SGX.sol");
 
 module.exports = function (deployer) {
-    deployer.deploy(ERCXXX_SGX, "BTC-ERC", "BTH", 1);
+    if (network == "development") {
+        deployer.deploy(ERCXXX_SGX, "BTC-ERC", "BTH", 1);
+    } else {
+        // Perform a different step otherwise.
+    }
 };
