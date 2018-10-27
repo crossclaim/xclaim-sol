aa5329fd6cc99b1319ec4f92b6f76d692361dc87
Dominik Harz <dominik.harz@gmail.com>
2018-10-27 11:53:43 +0100

    add local geth node

--- truffle.js
+++ truffle.js
@@ -1,7 +1,7 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
 
-var infura_apikey = "xxx";
-var mnemonic = "xxx";
+var infura_apikey = "a311240a97d84b93b201e772187be620";
+var mnemonic = "alert pigeon review syrup similar narrow angle mobile purse breeze cream length";
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -16,8 +16,12 @@ module.exports = {
       network_id: 3,
       provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + infura_apikey),
       gas: 5800000
+    },
+    geth: {
+      host: "localhost",
+      port: 8545,
+      gas: 5800000
     }
-
   },
   mocha: {
     enableTimeouts: false
