0970867d7a7c9b52756803814e187e5b78df9cd3
Dominik Harz <dominik.harz@gmail.com>
2018-11-01 04:58:50 +0000

    Match contract and interface

--- contracts/ERCXXX_SGX_Interface.sol
+++ contracts/ERCXXX_SGX_Interface.sol
@@ -1,31 +1,12 @@
 pragma solidity ^0.4.24;
-import "./ERCXXX_Interface.sol";
 
 /**
 * ERCXXX using SGX issuers Interface
 */
-contract ERCXXX_SGX_Interface is ERCXXX_Interface {
-
-    // #####################
-    // CONTRACT VARIABLES
-    // #####################
-
-    /**
-    * List of trusted SGX issuers
-    */
-    // mapping(address => uint) issuers;
-
-
-    // #####################
-    // MODIFIERS
-    // #####################
+contract ERCXXX_SGX_Interface {
 
     // #####################
     // FUNCTIONS
-    // #####################
-
-    // #####################
-    // HELPER FUNCTIONS
     // #####################
 
     // #####################
