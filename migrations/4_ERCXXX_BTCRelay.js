a4555be8d4c3e25368f9f3a985c165cfed28496a
Dominik Harz <dominik.harz@gmail.com>
2018-11-01 08:23:37 +0000

    network config

--- migrations/4_ERCXXX_BTCRelay.js
+++ migrations/4_ERCXXX_BTCRelay.js
@@ -1,14 +1,12 @@
 var BTCRelay = artifacts.require("./BTCRelay/BTCRelay.sol");
 var ERCXXX_BTCRelay = artifacts.require("./impl/ERCXXX_BTCRelay.sol");
 
-module.exports = function (deployer) {
+module.exports = function (deployer, network) {
     if (network == "development") {
         deployer.deploy(BTCRelay).then(function () {
             return deployer.deploy(ERCXXX_BTCRelay, BTCRelay.address);
         })
-    } else if (network== "") {
-
-        // Perform a different step otherwise.
+    } else if (network == "ropsten") {
+        // Use existing deployed contracts
     }
-
 };
--- migrations/6_ERCXXX_SGXRelay.js
+++ migrations/6_ERCXXX_SGXRelay.js
@@ -1,6 +1,6 @@
 var ERCXXX_SGXRelay = artifacts.require("./impl/ERCXXX_SGXRelay.sol");
 
-module.exports = function (deployer) {
+module.exports = function (deployer, network) {
     if (network == "development") {
         deployer.deploy(ERCXXX_SGXRelay);
     } else {
