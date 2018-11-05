64bd7951b12e35294cc9b2239d38a20bbc78c966
Dominik Harz <dominik.harz@gmail.com>
2018-11-05 07:57:51 +0000

    include gas cost calculation

--- test/helpers.js
+++ test/helpers.js
@@ -12,7 +12,10 @@ module.exports = {
         }
     },
     convertToUsd: function (gasCost) {
+        // gas price conversion
+        const gas_price = web3.toWei(5, "gwei");
+        const eth_usd = 409; // USD
+
         return gasCost * web3.fromWei(gas_price, "ether") * eth_usd;
     }
 };
-
