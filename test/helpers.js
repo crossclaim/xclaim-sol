00330bf51d6f4c3c6e825126523b01c4b521f658
Dominik Harz <dominik.harz@gmail.com>
2018-11-22 04:31:34 +0000

    adjust gas price and usd etheruem conversion

--- test/helpers.js
+++ test/helpers.js
@@ -13,8 +13,8 @@ module.exports = {
     },
     convertToUsd: function (gasCost) {
         // gas price conversion
-        const gas_price = web3.toWei(5, "gwei");
-        const eth_usd = 409; // USD
+        const gas_price = web3.toWei(9, "gwei");
+        const eth_usd = 127; // USD
 
         return gasCost * web3.fromWei(gas_price, "ether") * eth_usd;
     }
