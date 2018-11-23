95d76ca657b5a67a0f3c3df23d46e2eff355707c
Dominik Harz <dominik.harz@gmail.com>
2018-11-23 02:30:39 +0000

    include a config

---
+++ scripts/btcrelay-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    networks: {
+        development: {
+            address: "0x28e335e4bc3d62f6be2d1b1dcb67cbc65aa0d40b",
+        },
+        ropsten: {
+            address: "",
+        },
+        main: {
+            address: "",
+        }
+    }
+}
