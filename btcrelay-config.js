dd3db2d57d1f55dc6dbb48bb0616d43d21ffa0ea
Dominik Harz <dominik.harz@gmail.com>
2019-01-04 14:08:23 +0000

    include ropsten and mainnet btcrelay

---
+++ btcrelay-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    networks: {
+        ropsten: {
+            address: "0x0e5f7a8d8b9dc3e9c0607a671d18a1d6b673f568",
+        },
+        main: {
+            address: "0x41f274c0023f83391de4e0733c609df5a124c3d4",
+        }
+    }
+}
