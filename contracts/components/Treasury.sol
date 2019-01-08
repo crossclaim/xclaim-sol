0fbbcd933995ce5044617d0df285bf2e6a0657cd
Dominik Harz <dominik.harz@gmail.com>
2019-01-08 19:21:59 +0000

    replace timelocks with number of blocks

--- contracts/components/Treasury.sol
+++ contracts/components/Treasury.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity ^ 0.5 .0;
 
 import "../interfaces/Treasury_Interface.sol";
 import "../components/ERC20.sol";
@@ -21,20 +21,21 @@ contract Treasury is Treasury_Interface, ERC20 {
     // relayer
     address public _relayer;
 
-    // time
+    // block periods
+    uint256 public _confirmations;
     uint256 public _contestationPeriod;
     uint256 public _graceRedeemPeriod;
-    
+
     // collateral
     uint256 public _minimumCollateralIssuer;
     uint256 public _minimumCollateralUser;
-    
+
     // conversion rate
     uint256 public _conversionRateBTCETH; // 10*5 granularity?
 
     // issue
     struct CommitedCollateral {
-        uint256 commitTimeLimit;
+        uint256 blocknumber;
         uint256 collateral;
         bytes btcAddress;
     }
@@ -62,20 +63,21 @@ contract Treasury is Treasury_Interface, ERC20 {
     uint256[] public _redeemRequestList;
     uint256 public _redeemRequestId;
 
-    constructor () public {
+    constructor() public {
         _totalSupply = 0;
         // issuer
         _issuerTokenSupply = 0;
         _issuerCommitedTokens = 0;
         _issuerCollateral = 0;
-        // time
-        _contestationPeriod = 1 seconds;
-        _graceRedeemPeriod = 1 seconds;
+        // block
+        _confirmations = 12;
+        _contestationPeriod = 30;
+        _graceRedeemPeriod = 30;
         // collateral
         _minimumCollateralUser = 1 wei;
         _minimumCollateralIssuer = 1 wei;
         // conversion rate
-        _conversionRateBTCETH = 2 * 10^5; // equals 1 BTC = 2 ETH
+        _conversionRateBTCETH = 2 * 10 ^ 5; // equals 1 BTC = 2 ETH
         // init id counters
         _tradeOfferId = 0;
         _redeemRequestId = 0;
@@ -101,7 +103,7 @@ contract Treasury is Treasury_Interface, ERC20 {
     // ---------------------
     // SETUP
     // ---------------------
-    function getEthtoBtcConversion() public returns (uint256) {
+    function getEthtoBtcConversion() public returns(uint256) {
         return _conversionRateBTCETH;
     }
 
@@ -163,21 +165,27 @@ contract Treasury is Treasury_Interface, ERC20 {
         require(msg.value >= _minimumCollateralUser, "Collateral too small");
         require(_issuerTokenSupply > amount + _issuerCommitedTokens, "Not enough collateral provided by issuer");
 
-        uint256 timelock = now + 60 seconds;
         _issuerCommitedTokens += amount;
-        _userCommitedCollateral[msg.sender] = CommitedCollateral(timelock, amount, btcAddress);
-        
+        _userCommitedCollateral[msg.sender] = CommitedCollateral(block.number, amount, btcAddress);
+
         // TODO: need to lock issuers collateral
-        
+
         // emit event
-        emit RegisterIssue(msg.sender, amount, timelock);
+        emit RegisterIssue(msg.sender, amount, block.number);
     }
 
     function issueToken(address receiver, uint256 amount, bytes memory data) public {
         require(_userCommitedCollateral[receiver].collateral > 0, "Collateral too small");
 
-        // check if within time delta
-        bool time_limit = (_userCommitedCollateral[receiver].commitTimeLimit > now);
+        // check if within number of blocks
+
+        bool confirmed;
+        if ((_userCommitedCollateral[receiver].blocknumber >= (block.number - _confirmations)) 
+        && (_userCommitedCollateral[receiver].blocknumber <= (block.number + _contestationPeriod))) {
+            confirmed = true;
+        } else {
+            confirmed = false;
+        }
 
         // BTCRelay verifyTx callback
         bool tx_valid = _verifyTx(data);
@@ -185,13 +193,13 @@ contract Treasury is Treasury_Interface, ERC20 {
         // TODO: match btc and eth address
         bool address_valid = _verifyAddress(receiver, _userCommitedCollateral[receiver].btcAddress, data);
 
-        if (time_limit && tx_valid && address_valid) {
+        if (confirmed && tx_valid && address_valid) {
             // issue tokens
             _totalSupply += amount;
             _balances[receiver] += amount;
             // reset user issue
-            _userCommitedCollateral[msg.sender].collateral = 0; 
-            _userCommitedCollateral[msg.sender].commitTimeLimit = 0;
+            _userCommitedCollateral[msg.sender].collateral = 0;
+            _userCommitedCollateral[msg.sender].blocknumber = 0;
 
             emit IssueToken(msg.sender, receiver, amount, data);
             return;
@@ -220,7 +228,7 @@ contract Treasury is Treasury_Interface, ERC20 {
 
         _balances[msg.sender] -= tokenAmount;
         _tradeOfferStore[_tradeOfferId] = TradeOffer(msg.sender, ethParty, tokenAmount, ethAmount, false);
-        
+
         emit NewTradeOffer(_tradeOfferId, msg.sender, tokenAmount, ethParty, ethAmount);
 
         _tradeOfferId += 1;
@@ -234,7 +242,7 @@ contract Treasury is Treasury_Interface, ERC20 {
         /* Complete the offer */
         _tradeOfferStore[offerId].completed = true;
         _balances[msg.sender] = _balances[msg.sender] + _tradeOfferStore[offerId].tokenAmount;
-        
+
         _tradeOfferStore[offerId].tokenParty.transfer(msg.value);
 
         emit Trade(offerId, _tradeOfferStore[offerId].tokenParty, _tradeOfferStore[offerId].tokenAmount, msg.sender, msg.value);
@@ -285,7 +293,7 @@ contract Treasury is Treasury_Interface, ERC20 {
         _balances[redeemer] -= _redeemRequestMapping[id].value;
 
         redeemer.transfer(_redeemRequestMapping[id].value);
-        
+
         emit Reimburse(redeemer, _issuer, _redeemRequestMapping[id].value);
     }
 
@@ -325,7 +333,7 @@ contract Treasury is Treasury_Interface, ERC20 {
         _issuer.transfer(_issuerCollateral);
 
         emit ExecuteReplace(_issuerCandidate, _issuerCollateral);
-    }       
+    }
 
     function abortReplace() public {
         require(_issuerReplace);
@@ -343,7 +351,7 @@ contract Treasury is Treasury_Interface, ERC20 {
     // HELPERS
     // ---------------------
 
-    function _verifyTx(bytes memory data) private returns (bool verified) {
+    function _verifyTx(bytes memory data) private returns(bool verified) {
         // data from line 256 https://github.com/ethereum/btcrelay/blob/develop/test/test_btcrelay.py
         bytes memory rawTx = "0x8c14f0db3df150123e6f3dbbf30f8b955a8249b62ac1d1ff16284aefa3d06d87";
         uint256 txIndex = 0;
@@ -352,7 +360,7 @@ contract Treasury is Treasury_Interface, ERC20 {
         merkleSibling[1] = uint256(sha256("0x8e30899078ca1813be036a073bbf80b86cdddde1c96e9e9c99e9e3782df4ae49"));
         uint256 blockHash = uint256(sha256("0x0000000000009b958a82c10804bd667722799cc3b457bc061cd4b7779110cd60"));
 
-        (bool success, bytes memory returnData)  = _relayer.call(abi.encodeWithSignature("verifyTx(bytes, uint256, uint256[], uint256)", rawTx, txIndex, merkleSibling, blockHash));
+        (bool success, bytes memory returnData) = _relayer.call(abi.encodeWithSignature("verifyTx(bytes, uint256, uint256[], uint256)", rawTx, txIndex, merkleSibling, blockHash));
 
         // TODO: Implement this correctly, now for testing only
         if (data.length == 0) {
@@ -362,7 +370,7 @@ contract Treasury is Treasury_Interface, ERC20 {
         }
     }
 
-    function _verifyAddress(address receiver, bytes memory btcAddress, bytes memory data) private returns (bool verified) {
+    function _verifyAddress(address receiver, bytes memory btcAddress, bytes memory data) private returns(bool verified) {
         return true;
     }
 
