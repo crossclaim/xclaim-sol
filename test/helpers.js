0c28fb8b2611191318a64c26c422922dbc0376a9
Dominik Harz <dominik.harz@gmail.com>
2019-01-16 12:53:06 +0000

    Add fun to forward blocks in ganache and remove eventFired helper fun

--- test/helpers.js
+++ test/helpers.js
@@ -1,21 +1,26 @@
 module.exports = {
-    eventFired: function (transaction, eventName) {
-        for (var i = 0; i < transaction.logs.length; i++) {
-            var log = transaction.logs[i];
-            if (log.event == eventName) {
-                // We found the event!
-                assert.isTrue(true);
-            }
-            else {
-                assert.isTrue(false, "Did not find " + eventName);
-            }
-        }
-    },
     convertToUsd: function (gasCost) {
         // gas price conversion
         const gas_price = web3.utils.toWei(9, "gwei");
         const eth_usd = 106; // USD
 
         return gasCost * web3.utils.fromWei(gas_price, "ether") * eth_usd;
+    },
+    generateBlocksGanache: function(number) {
+        return new Promise((resolve, reject) => {
+        // ganache uses evm_mine method to generate new blocks
+            for (var i = 0; i < number; i++) {
+                web3.currentProvider.send({
+                    jsonrpc: "2.0",
+                    method: "evm_mine",
+                    id: 123
+                    }, (err, result) => {
+                        if (err) { return reject(err); }
+                        const newBlockHash = web3.eth.getBlock('latest').hash;
+            
+                        return resolve(newBlockHash)
+                    });
+            }
+        });
     }
 };
